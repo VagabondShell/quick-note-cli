4db77c09b6370bd719f79aa09f3ac169de445ebf
sait-nac <sait-nac@student.1337.ma>
2026-03-12 05:43:05 +0000

    basic starting file structer with basic db management utils

---
+++ src/db.js
@@ -0,0 +1,20 @@
+import fs from 'node:fs/promises'
+
+const dbPath = '../db.json';
+export const getDb = async () => {
+    const db = await fs.readFile(dbPath, 'utf8');
+    return JSON.parce(db);
+}
+
+export const saveDb = async (db) => {
+    await fd.writeFile(dbPath, JSON.stringify(db, null, 2), 'utf8');
+    return db;
+}
+
+export const insetDb = async (note) => {
+    const db = await getDb();
+    db.notes.push(note);
+    await saveDb(db);
+    return data;
+}
+
